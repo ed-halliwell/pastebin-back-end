cb3961bafd5e0cde727bde9787c16ae1e4b87f72
Ed Halliwell <edhalliwell@hotmail.com>
2021-12-10 16:59:35 +0000

    Tests for GET endpoint added

--- server.test.ts
+++ server.test.ts
@@ -2,9 +2,32 @@ import supertest from "supertest";
 import app, { client, server } from "./server";
 
 describe("GET /snippets", () => {
-  it("retrieves all the snippets store in the snippets table", async () => {
+  test("retrieves all the snippets store in the snippets table", async () => {
     const response = await supertest(app).get("/snippets");
+    expect(response.status).toEqual(200);
     expect(response.body.message).toMatch("Retrieved snippets");
+  });
+  test("retrieves specific number of snippets", async () => {
+    const response = await supertest(app).get("/snippets?limit=4");
+    expect(response.body.data.length).toEqual(4);
+    expect(response.body.message).toMatch("Retrieved snippets");
+    expect(response.status).toEqual(200);
+  });
+  test("if limit over 100, returns 100 or fewer snippets", async () => {
+    const response = await supertest(app).get("/snippets?limit=101");
+    expect(response.status).toEqual(200);
+    expect(response.body.message).toMatch("Retrieved snippets");
+    expect(response.body.data.length).toBeLessThanOrEqual(100);
+  });
+  test("if limit is 0 or less, returns 400 response", async () => {
+    const response = await supertest(app).get("/snippets?limit=-1");
+    expect(response.status).toEqual(400);
+    expect(response.body.message).toMatch("Bad request");
+  });
+  test("if limit is not a number, returns 400 response", async () => {
+    const response = await supertest(app).get("/snippets?limit=hello");
+    expect(response.status).toEqual(400);
+    expect(response.body.message).toMatch("Bad request");
   });
 });
 
