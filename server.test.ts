b062084b6fb4efb17e5badf92191dec376a7767d
Ed Halliwell <edhalliwell@hotmail.com>
2021-12-13 08:14:40 +0000

    Added some tests for GET endpoints

--- server.test.ts
+++ server.test.ts
@@ -13,21 +13,38 @@ describe("GET /snippets", () => {
     expect(response.body.message).toMatch("Retrieved snippets");
     expect(response.status).toEqual(200);
   });
-  test("if limit over 100, returns 100 or fewer snippets", async () => {
+  test("returns 100 or fewer snippets if limit query param is over 100", async () => {
     const response = await supertest(app).get("/snippets?limit=101");
     expect(response.status).toEqual(200);
     expect(response.body.message).toMatch("Retrieved snippets");
     expect(response.body.data.length).toBeLessThanOrEqual(100);
   });
-  test("if limit is 0 or less, returns 400 response", async () => {
+  test("returns 400 response if limit is 0 or less", async () => {
     const response = await supertest(app).get("/snippets?limit=-1");
     expect(response.status).toEqual(400);
     expect(response.body.message).toMatch("Bad request");
   });
-  test("if limit is not a number, returns 400 response", async () => {
+  test("returns 400 response if limit is not a number", async () => {
     const response = await supertest(app).get("/snippets?limit=hello");
     expect(response.status).toEqual(400);
     expect(response.body.message).toMatch("Bad request");
+  });
+});
+
+describe("GET /snippets/1", () => {
+  test("retrieves one snippets from the snippets table", async () => {
+    const response = await supertest(app).get("/snippets/1");
+    expect(response.status).toEqual(200);
+    expect(response.body.message).toMatch("Retrieved snippet");
+    expect(response.body.data).toHaveLength(1);
+  });
+  test("if snippet cannot be found, an error is returned", async () => {
+    const response = await supertest(app).get("/snippets/0");
+    expect(response.status).toEqual(404);
+    expect(response.body.message).toStrictEqual(
+      "Could not find a snippet with that ID."
+    );
+    expect(response.body.data).toStrictEqual({});
   });
 });
 
